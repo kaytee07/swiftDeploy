aa05fdc38c0b3e9d1d6155b978a36ab57c586552
kaytee07 <kofi.taylor07@gmail.com>
2023-08-21 14:25:23 +0000

    attribute common to all classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,67 +1,37 @@
 #!/usr/bin/python3
 """
-this class object defines common attributes and models like the id
-, time of creation and time of update which will be a part of all
-models we create
+defines the common attribute for all other classes 
 """
 import uuid
-import sys
 from datetime import datetime
-import models
-from sqlalchemy import Column, Integer, String, create_engine, Datetime
-from sqlalchemy.ext.declarative import declarative_base
+
+time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
-Base = declarative_base()
-
-
-class BaseModel():
+class BaseModel:
     """
-    this class object defines common attributes and models
+    attributes common to all other classes
     """
-    id = Column(String(60), primary_key=True)
-    created_at = Column(Datetime, default=datetime.utcnow)
-    updated_at = Column(Datetime, default=datetime.utcnow)
-
-    def __init__(self, *args, **kwargs):
-        """initialize class attributes"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        date_obj = datetime.strptime(value, time)
-                        setattr(self, key, date_obj)
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+    def __init__(self):
+        """
+        initialize class attributes
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
-        """Represent class in a string form"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """update the updated_at atribute"""
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-
-    def delete(self):
-        """delete current instance from storage"""
-        models.storage.delete(self)
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-        """
-        return dictionary containing all key values of __dict__
-        of the instance with new key class
-        """
         new_dict = {}
         new_dict['__class__'] = self.__class__.__name__
         for key, value in self.__dict__.items():
             if key == 'updated_at' or key == 'created_at':
-                new_dict[key] = value.strftime(time)
+                new_dict[key] = value.strftime(time_fmt)
             else:
                 new_dict[key] = value
         return new_dict
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """
 User class that inherit from BaseModel
 """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -12,11 +13,18 @@ class User(Base, BaseModel):
     User models important details about our user object
     """
     __tablename__ = "users"
+    SD_TYPE_STORAGE = getenv('SD_TYPE_STORAGE')
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    if SD_TYPE_STORAGE == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     container = relationship('Container', back_populates='user')
 

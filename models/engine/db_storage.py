08b3826ca96e3d8e00fcb45b95871a5409e7f207
kaytee07 <kofi.taylor07@gmail.com>
2023-08-22 14:20:05 +0000

    database storage class

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,29 +1,86 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
-store object value in database
+store attributes in database
 """
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.container import Container
 
-class DBStorage():
+classes = {'User': User, 'Container': Container}
+
+classes
+
+
+class DBStorage:
     """
-    CRUD using database tables
+    store object attribute in database
     """
     __engine = None
-    _session = None
+    __session = None
 
     def __init__(self):
-        """
-        create session engine and link it to the database
-        """
         SD_MYSQL_USER = getenv('SD_MYSQL_USER')
         SD_MYSQL_PWD = getenv('SD_MYSQL_PWD')
         SD_MYSQL_HOST = getenv('SD_MYSQL_HOST')
         SD_MYSQL_DB = getenv('SD_MYSQL_DB')
         SD_ENV = getenv('SD_ENV')
-        self.__engine = create_engine("mysql+mysqldb://{}.{}@{}/{}"
-                                      .format(SD_MYSQL_USER,
-                                              SD_MYSQL_PWD,
-                                              SD_MYSQL_HOST,
-                                              SD_MYSQL_DB))
+
+        dburl = "mysql+mysqldb://{}.{}@{}/{}".format(SD_MYSQL_USER,
+                                                     SD_MYSQL_PWD,
+                                                     SD_MYSQL_HOST,
+                                                     SD_MYSQL_DB)
+
+        self.__engine = create_engine(dburl, pool_pre_ping=True)
+
+        if SD_ENV == 'test':
+            Base.metadata.drop_all()
+
+    def all(self, cls=None):
+        """
+        get all object based on their class name and if cls = None
+        query all object types in Database
+        """
+        new_obj = {}
+        if cls:
+            objects = self.__session.query(cls).all()
+            for obj in objects:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                new_obj[key] = obj
+
+        else:
+            for value in classes.values():
+                objects = self.__session.query(value).all()
+                for obj in objects:
+                    key = f"{obj.__class__.__name__}.{obj.id}"
+                    new_obj[key] = obj
+        return new_obj
+
+    def new(self, obj=None):
+        """
+        add object to the session 
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+        commit all changes to the current database session
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        delete object from the current database session
+        """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        create tables in database and create database session
+        """
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()

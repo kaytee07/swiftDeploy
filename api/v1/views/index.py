1ec69718abc7840b5bcea755173508ceee56aab0
kaytee07 <kofi.taylor07@gmail.com>
2023-08-23 13:42:10 +0000

    get database start of every object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,19 @@
 create route for our app_views blue print
 """
 from api.v1.views import app_views
+from models import storage
+from models.user import User
+from models.container import Container
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     return {"status": "OK"}
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    return {
+        "users": storage.count(User),
+        "container": storage.count(Container)
+    }

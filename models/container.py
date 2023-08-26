20eb12e780108d33113680aea40258969c3991bb
kaytee07 <kofi.taylor07@gmail.com>
2023-08-26 11:19:15 +0000

    update database models

--- models/container.py
+++ models/container.py
@@ -2,20 +2,35 @@
 """
 docker container class with it attribute
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from os import getenv
+
+SD_TYPE_STORAGE = getenv('SD_TYPE_STORAGE')
 
 
-class Container(BaseModel):
+class Container(BaseModel, Base):
     """
     Container objects having all the necessart methods and
     attributes
     """
-    status = None
-    container_id = None
-    start_time = None
-    stop_time = None
-    user_id = None
+    __tablename__ = 'containers'
+
+    if SD_TYPE_STORAGE == 'db':
+        name = Column(String(60), nullable=False)
+        status = Column(String(60), nullable=False)
+        container_id = Column(String(64), nullable=False)
+        start_time = Column(DateTime)
+        stop_time = Column(DateTime)
+        user_id = Column(String(60), ForeignKey('users.id'))
+    else:
+        name = None
+        status = None
+        container_id = None
+        start_time = None
+        stop_time = None
+        user_id = None
 
     def __init__(self, *args, **kwargs):
         """
--- models/user.py
+++ models/user.py
@@ -2,18 +2,34 @@
 """
 creat user class that inherit from BaseModel
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
+SD_TYPE_STORAGE = getenv('SD_TYPE_STORAGE')
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
     user class that describes the user
     """
-    first_name = None
-    last_name = None
-    username = None
-    email = None
-    password = None
+    __tablename__ = 'users'
+
+    if SD_TYPE_STORAGE == 'db':
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        username = Column(String(60), nullable=False)
+        email = Column(String(60), nullable=False)
+        password = Column(String(60), nullable=False)
+        salt = Column(String(128), nullable=False)
+        container = relationship('Container', backref='user')
+    else:
+        first_name = None
+        last_name = None
+        username = None
+        email = None
+        salt = None
+        password = None
 
     def __init__(self, *args, **kwargs):
         """

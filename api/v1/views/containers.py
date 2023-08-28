59fe01b766e39760638b12e384210d184dbc6560
kaytee07 <kofi.taylor07@gmail.com>
2023-08-28 14:07:04 +0000

    login onto docker on server using script written on server

--- api/v1/views/containers.py
+++ api/v1/views/containers.py
@@ -4,26 +4,40 @@ start, stop containers and pull images
 """
 from models import storage
 from models.container import Container
+from paramiko import SSHException
 from models.user import User
 from api.v1.views import app_views
 from flask import abort, request, jsonify
 from fabric import Connection
 
 
+def login_to_docker(c, dockerID, password):
+    try:
+        result = c.run(f"./logintodockerhub {dockerID} {password}")
+        return result.stdout
+    except SSHException as e:
+        return {"error": "SSH connection error: " + str(e)}, 500
+    except Exception as e:
+        return {"error": "An error occurred: " + str(e)}, 500
+
 @app_views.route('/containers/<username>/hublogin', strict_slashes=False, methods=['POST'])
 def login_dockerHub(username):
+    """
+    login into user's docker hub account with their docker from requests
+    and password they'll enter
+    """
     data = request.get_json()
     if not data:
         abort(400, description="Not a JSON")
     user = storage.get(User, username=username).to_dict()
     if user:
-        c = Connection(host="ubuntu@52.87.212.95")
-        result = c.run("ls projects")
+        conn = Connection(host="ubuntu@52.87.212.95")
+        result = login_to_docker(conn, user['dockerID'], data['password'])
 
         return jsonify({
             "dockerID": user['dockerID'],
             "hubpass": data["password"],
-            "message": result.stdout
+            "message": result
         }), 200
 
 

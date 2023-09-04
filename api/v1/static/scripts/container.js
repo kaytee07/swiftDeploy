79f0396729c2b041dd067e353f15a5a6781f2ce0
kaytee07 <kofi.taylor07@gmail.com>
2023-09-04 16:54:03 +0000

    js

--- api/v1/static/scripts/container.js
+++ api/v1/static/scripts/container.js
@@ -1,6 +1,8 @@
 const pullbtn = document.querySelector(".pull_cont button")
 const closebtn = document.querySelector("svg")
 const card = document.querySelector(".pull_card")
+const containerList = document.querySelector("ul.containers")
+
 
 
 pullbtn.addEventListener('click', () => {
@@ -11,11 +13,9 @@ closebtn.addEventListener('click', () => {
   card.style.display = 'none';
 });
 
-console.log(id)
-console.log(username)
 
-
-fetch(`http://localhost:5001/api/v1/containers/${username}`)
+function getAllContainers(usernme) {
+fetch(`http://localhost:5001/api/v1/containers/${usernme}`)
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -24,7 +24,27 @@ fetch(`http://localhost:5001/api/v1/containers/${username}`)
     })
     .then(data => {
         console.log(data);
+	let html = ""
+	const containers = Object.entries(data)
+	for (const [key, value] of containers) {
+	 html+= `
+               <li class="container">
+               <h5>${value.name}</h5>
+               <h5>${value.container_id}</h5>
+               <h5>${value.status}</h5>
+               <button>${value.status === "stopped" ? "start" : "stop"}</button>
+               <button>Open</button>
+               </li>
+               `;  
+	}
+	if (html) {
+	   containerList.innerHTML = html
+	}
     })
     .catch(error => {
         console.error('Fetch error:', error);
     });
+}
+
+getAllContainers(username)
+

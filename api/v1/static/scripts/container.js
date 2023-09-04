d35d0327e79c0ec30253ab1503ef33aad6916f6e
kaytee07 <kofi.taylor07@gmail.com>
2023-09-04 12:23:04 +0000

    add container

--- api/v1/static/scripts/container.js
+++ api/v1/static/scripts/container.js
@@ -2,6 +2,7 @@ const pullbtn = document.querySelector(".pull_cont button")
 const closebtn = document.querySelector("svg")
 const card = document.querySelector(".pull_card")
 
+
 pullbtn.addEventListener('click', () => {
   card.style.display = 'flex';
 });
@@ -9,3 +10,21 @@ pullbtn.addEventListener('click', () => {
 closebtn.addEventListener('click', () => {
   card.style.display = 'none';
 });
+
+console.log(id)
+console.log(username)
+
+
+fetch(`http://localhost:5001/api/v1/containers/${username}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });

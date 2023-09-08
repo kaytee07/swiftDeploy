607bdf6bc013ba7af596017b7d33e1236c7a3a3e
kaytee07 <kofi.taylor07@gmail.com>
2023-09-08 12:52:01 +0000

    implemented database select

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,14 @@
 """
 create instance of file storage and deserialze json file
 """
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
-storage.reload()
+from os import getenv
+
+SD_TYPE_STORAGE = getenv('SD_TYPE_STORAGE')
+if SD_TYPE_STORAGE == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
